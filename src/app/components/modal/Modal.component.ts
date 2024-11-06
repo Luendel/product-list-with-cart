a3d7937b61c98270245cb113b4e960d0605c77f3
Luendel <luendel3@hotmail.com>
2024-11-06 11:28:19 -0300

    modified:   src/app/app.component.html modified:   src/app/components/modal/Modal.component.css modified:   src/app/components/modal/Modal.component.html modified:   src/app/components/modal/Modal.component.ts

--- src/app/components/modal/Modal.component.ts
+++ src/app/components/modal/Modal.component.ts
@@ -1,10 +1,51 @@
-import { Component } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
+import { GetProducts } from "../../services/GetProducts/GetProducts.service";
+import { Subscription } from "rxjs";
 
 @Component({
     selector:"app-modal",
     templateUrl:"./Modal.component.html",
     styleUrl:"./Modal.component.css"
 })
-export class Modal {
+export class Modal implements OnInit{
+    @Input() totalPrice:any;
+    @Input() items:any;
+
+    class = "none"
+
+    products:any;
+
+    subscription!: Subscription;
+
+    constructor(private get_products:GetProducts){}
+
+    ngOnInit(): void {
+        let temp:any;
+
+        this.subscription = this.get_products.getProducts().subscribe(function(item:any):void{
+            temp = item
+        })
+
+        this.products = temp
+    }
+
+    handleClick(){
+        console.log(this.products)
+    }
+
+    handleSrc(name:string){
+        let temp = this.products
+        let i = 0
+
+        while(temp[i].name !== name){
+            i++
+
+            if(i > temp.length){
+                break
+            }
+        }
+
+        return temp[i].image.thumbnail
+    }
 
 }
